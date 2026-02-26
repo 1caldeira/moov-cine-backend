1368bd00e015081107b1c7e57403181c8c2eb8d4
1caldeira <g.caldeiraa1@gmail.com>
2026-02-26 18:24:12 -0300

    fix: Correçao mixed content

--- Program.cs
+++ Program.cs
@@ -125,6 +125,20 @@ builder.Services.AddIdentity<Usuario, IdentityRole>()
     .AddDefaultTokenProviders()
     .AddErrorDescriber<PortuguesIdentityErrorDescriber>();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Events.OnRedirectToLogin = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return Task.CompletedTask;
+    };
+    options.Events.OnRedirectToAccessDenied = context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        return Task.CompletedTask;
+    };
+});
+
 var app = builder.Build();
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseRouting();
