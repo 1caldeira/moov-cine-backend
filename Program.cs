193484a1c5d4fe11bb0747b4605495159401270d
1caldeira <g.caldeiraa1@gmail.com>
2026-02-26 12:52:55 -0300

    fix: libera CORS para vercel

--- Program.cs
+++ Program.cs
@@ -114,7 +114,9 @@ builder.Services.AddCors(options =>
                 "https://localhost:7015",
                 "http://localhost:5500",
                 "http://localhost:5501",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "http://moovcine.vercel.app",
+                "https://moovcine.vercel.app"
                )
               .AllowAnyMethod()
               .AllowAnyHeader()
